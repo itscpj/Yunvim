be00a3fc7fa54c7da23cd654e96750b95252ef57
cpj <pyjac@proton.me>
2024-10-08 12:05:08 +0800

    Automatically change themes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,18 @@
+local function get_theme()
+    -- 获取当前时间
+    local hour = tonumber(os.date('%H'))
+    
+    -- 判断时间并返回相应的主题
+    if hour >= 8 and hour < 17 then
+        return 'onelight'
+    else
+        return 'palenight'
+    end
+end
+
+-- 设置lualine，使用get_theme函数来动态选择主题
 require('lualine').setup({
     options = {
-        theme = 'palenight'
+        theme = get_theme()
     }
 })
